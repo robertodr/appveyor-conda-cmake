77d0c577e85f2c80022a17976d3c5e59006be795
Radovan Bast <bast@users.noreply.github.com>
2018-09-18 22:18:02 +0200

    more output

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import sys
+import subprocess
+import shlex
+
+command = 'ls -l'
+
+cmd = shlex.split(command)
+
+result = subprocess.run(cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        universal_newlines=True)
+
+print('stdout', result.stdout)
+print('stderr', result.stderr)
 
 sys.exit(0)
